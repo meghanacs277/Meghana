0c0f48576a822c9ca5d75845cbb165b595319f8a
meghanacs277 <92203957+meghanacs277@users.noreply.github.com>
2021-10-09 13:54:35 +0530

    Add files via upload

---
+++ Method overloading/CS.java
@@ -0,0 +1,15 @@
+class CS{
+static void player(){
+System.out.println("there are 11 players");
+System.out.println("RCB rocks");
+}
+static void player(String name){
+System.out.println(name);
+System.out.println("Thanu");
+}
+static void player(int a,int b){
+System.out.println(a);
+System.out.println(b);
+}
+
+}
---
+++ Method overloading/IPL2.java
@@ -0,0 +1,8 @@
+class IPL1{
+public static void main(String []ar){
+
+CS.player();
+CS.palyer("Dhoni");
+CS.player(1,2);
+}
+}
