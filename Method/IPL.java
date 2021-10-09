88c659be16b645441c85728b8cda2ded3a9331d3
meghanacs277 <92203957+meghanacs277@users.noreply.github.com>
2021-10-09 13:53:34 +0530

    Add files via upload

---
+++ Method/IPL.java
@@ -0,0 +1,8 @@
+class IPL{
+public static void main(String []args){
+MI.players();
+MI.name("sachin");
+int R=MI.iplyrnow();
+System.out.println(R);
+}
+}
---
+++ Method/MI.java
@@ -0,0 +1,18 @@
+class MI{
+static void players(){
+System.out.println("there are 11 players");
+System.out.println("RCB rocks");
+}
+static void name(String name){
+System.out.println(name);
+System.out.println("Thanu");
+}
+static int iplyrnow(){
+return 2021;
+}
+     
+}
+
+
+
+
